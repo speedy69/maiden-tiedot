795400be0f0c44cc5941ea414b6d9285e8bee557
Seppo Suikki <seppowass@gmail.com>
2021-08-15 22:10:53 +0300

    2.12

--- src/App.js
+++ src/App.js
@@ -1,9 +1,51 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 
+const Countries = ({filsu, lista}) => {
+  const temp = lista.filter(a => a.name.toLowerCase().includes(filsu.toLowerCase()))
+
+  if(temp.length === 1){
+    const test = temp[0]
+    return(
+      <div>
+        <h2>{test.name}</h2>
+        <p>capital {test.capital} <br/>
+        population {test.population}</p>
+        <h3>languages</h3>
+        <ul>
+          {test.languages.map(a => <li key={a.name}>{a.name}</li>)}
+        </ul>
+        <img alt='' src={test.flag} width='100' height='75'/>
+      </div>
+    )
+  }
+  if(temp.length < 20){
+    return(
+      <div>
+        {temp.map(a => <p key={a.name}>{a.name}<button>show</button></p>)}
+      </div>
+    )
+  }
+
+  return(
+    <div>Too many matches, specify another filter</div>
+  )
+}
 
 function App() {
+  const [maat, setMaat] = useState([])
+  const [filtteri, setFiltteri] = useState('')
+
+  useEffect(() => {
+    axios.get('https://restcountries.eu/rest/v2/all').then(r => setMaat(r.data))
+  }, [])
+
+  const handleChange = (e) => setFiltteri(e.target.value)
+
   return (
     <div >
-      hello world
+      find countries <input value={filtteri} onChange={handleChange} />
+      <Countries filsu={filtteri} lista={maat} />
     </div>
   );
 }
